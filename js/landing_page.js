9196788171426b94736158116f2b44771345b33d
Gergely Kovacs <rinceart@gmail.com>
2022-05-04 23:23:13 +0200

    v0.5.1.
    Mobile menu more or less done.

--- js/landing_page.js
+++ js/landing_page.js
@@ -1,59 +1,34 @@
-// When the event DOMContentLoaded occurs, it is safe to access the DOM
 document.addEventListener('DOMContentLoaded', function() {
-
-  // When the user scrolls the page, execute myFunction
   window.onscroll = function() {userScroll()};
 
-  // Get the header
-  var header = document.getElementById("headerNav");
-
-  // Get the offset position of the navbar
+  var header = document.getElementById("topNav");
   var sticky = header.offsetTop;
 
-  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
   function userScroll() {
     if (window.pageYOffset > sticky) {
-      header.classList.add("navigation_sticky");
+      header.classList.add("top_nav_filled");
     } else {
-      header.classList.remove("navigation_sticky");
+      header.classList.remove("top_nav_filled");
     }
   } 
-})
 
-document.addEventListener('DOMContentLoaded', function() {
   window.onresize = function() {hideMobileMenu()};
 
-  var mobileMenu = document.getElementById("mobileMenu");
-  var btnClose = document.getElementById("btnClose");
+  var mobileNav = document.getElementById("mobileNav");
 
-  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
   function hideMobileMenu() {
     if (window.innerWidth > 767) {
-      mobileMenu.style.display = "none";
-      btnClose.style.display = "none";
-      btnMenu.style.display = "none";
-    } else {
-      btnMenu.style.display = "block";
+      mobileNav.style.display = "none";
     }
   } 
 })
 
-function openMobileMenu() {
-  var mobileMenu = document.getElementById("mobileMenu");
-  var btnClose = document.getElementById("btnClose");
-  var btnMenu = document.getElementById("btnMenu");
-
-  mobileMenu.style.display = "flex";
-  btnClose.style.display = "block";
-  btnMenu.style.display = "none";
+function openMobileNav() {
+  var mobileNav = document.getElementById("mobileNav");
+  mobileNav.style.display = "flex";
 }
 
-function closeMobileMenu() {
-  var mobileMenu = document.getElementById("mobileMenu");
-  var btnClose = document.getElementById("btnClose");
-  var btnMenu = document.getElementById("btnMenu");
-
-  mobileMenu.style.display = "none";
-  btnClose.style.display = "none";
-  btnMenu.style.display = "block";
+function closeMobileNav() {
+  var mobileNav = document.getElementById("mobileNav");
+  mobileNav.style.display = "none";
 }
