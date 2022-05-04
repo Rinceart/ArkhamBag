99cdd83769e0c87f9e67de31dab08d37f155ec51
Gergely Kovacs <rinceart@gmail.com>
2022-05-04 16:35:44 +0200

    v0.5.
    Pretty god progress on the mobile menu.

--- js/landing_page.js
+++ js/landing_page.js
@@ -13,9 +13,47 @@ document.addEventListener('DOMContentLoaded', function() {
   // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
   function userScroll() {
     if (window.pageYOffset > sticky) {
-      header.classList.add("navbar_sticky");
+      header.classList.add("navigation_sticky");
     } else {
-      header.classList.remove("navbar_sticky");
+      header.classList.remove("navigation_sticky");
     }
   } 
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+  window.onresize = function() {hideMobileMenu()};
+
+  var mobileMenu = document.getElementById("mobileMenu");
+  var btnClose = document.getElementById("btnClose");
+
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function hideMobileMenu() {
+    if (window.innerWidth > 767) {
+      mobileMenu.style.display = "none";
+      btnClose.style.display = "none";
+      btnMenu.style.display = "none";
+    } else {
+      btnMenu.style.display = "block";
+    }
+  } 
+})
+
+function openMobileMenu() {
+  var mobileMenu = document.getElementById("mobileMenu");
+  var btnClose = document.getElementById("btnClose");
+  var btnMenu = document.getElementById("btnMenu");
+
+  mobileMenu.style.display = "flex";
+  btnClose.style.display = "block";
+  btnMenu.style.display = "none";
+}
+
+function closeMobileMenu() {
+  var mobileMenu = document.getElementById("mobileMenu");
+  var btnClose = document.getElementById("btnClose");
+  var btnMenu = document.getElementById("btnMenu");
+
+  mobileMenu.style.display = "none";
+  btnClose.style.display = "none";
+  btnMenu.style.display = "block";
+}
